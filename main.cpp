707edbb4c9eac596916fdd517a91150eca7eb9c5
tinekry <ssds95082@gmail.com>
2026-07-19 17:02:05 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,13 @@
 #include<bits/stdc++.h>
 
+// using std::string;
+// string init = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 0";
 
-using std::string;
-string init = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 0";
-
-class position {
-    string s;
-
-};
+// class position {
+//     string s;
+// };
 
 enum piece { p, P, n, N, b, B, r, R, q, Q, k, K, E };
-enum color { WHITE, BLACK, BOTH };
 enum cell {
     a8, b8, c8, d8, e8, f8, g8, h8, 
     a7, b7, c7, d7, e7, f7, g7, h7, 
@@ -36,35 +33,44 @@ struct board {
     std::set<int_fast64_t> hash_history;
     piece pos[64];
     int_fast32_t clock_halfmove = 0;
-    int_fast32_t clock_fullmove = 0;
+    // int_fast32_t clock_fullmove = 0;
     short enPass = -1;
     short castle = 15;
-
-    
-    // string pos;
-    // std::vector<positition> history;
     board() {
         for (int i = 0; i < 64; ++i) {
             pos[i] = E;
         }
     }
-    void init_classic () {
+    void init_classic() {
         std::copy(pos_classic, pos_classic + 64, pos);
     }
-    bool valid() {
-        return true;
-    }
-    /* 0-w 1-b */
-    bool is_under_check() {
-        return true;
-    }
+    // bool valid() {
+    //     return true;
+    // }
+    // bool is_under_check() {
+    //     return true;
+    // }
     bool move(short c1, short c2) {
         if (!(c1 >= 1 && c1 <= 64) 
            &&(c2 >= 1 && c2 <= 65)) return false;
         if (pos[c1] == E) return false;
+        short delta = c2 - c1;
+        short h = delta % 8;
+        short v = delta / 8;
         switch (pos[c1]) {
             case p:
-            case P:    
+            case P:
+                if (v == 0) return false;
+                if (clock_halfmove % 2 == 0 && v > 0) return false;
+                if (clock_halfmove % 2 == 1 && v < 0) return false;
+                if (abs(v) > 2) return false;
+                if (h != 0) {
+                    if (enPass == -1) return false;
+                    if (h != 1) return false;
+                    if (v != 1) return false;
+                } else {
+                    if (pos[c2] != E) return false;
+                }     
                 break;
             case n:
             case N:
@@ -77,15 +83,15 @@ struct board {
                 break;
             case k:
             case K:
-
+                break;
             default:
                 return false;
         }
         pos[c2] = pos[c1];
         pos[c1] = E;
+        clock_halfmove += 1;
+        // clock_fullmove += 1;
         return true;
-        clock_halfmove ^= 1;
-        clock_fullmove += 1;
 
     }
     // short parse(cell c1) {
